9d9da8cbb1d6ef529de8d101d1d04015bf9f2032
DD <aka@email.cm>
2020-12-05 12:39:12 +0100

    column and Row

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -27,7 +27,7 @@ class _HomeState extends State<Home> {
               color: Colors.white70,
               onPressed: null,
             ),
-            margin: EdgeInsets.fromLTRB(0 , 0, 0, 20.0),
+            margin: EdgeInsets.fromLTRB(0, 0, 0, 20.0),
           ),
         ),
         bottom: PreferredSize(
@@ -42,55 +42,57 @@ class _HomeState extends State<Home> {
           preferredSize: Size.fromHeight(75.0),
         ),
       ),
-      body: Padding(padding: EdgeInsets.all(16),
-      child: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              const ContainerWithBoxDecorationWidget(),
-            ],
-          ),
-        ),
-      )
-      ),
+      body: Padding(
+          padding: EdgeInsets.all(16),
+          child: SafeArea(
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  const ContainerWithBoxDecorationWidget(),
+                  Divider(),
+                  const ColumnWidget(),
+                  Divider(),
+                  const RowWidget(),
+                  Divider(),
+                  const ColumnAndRowNestingWidget(),
+                ],
+              ),
+            ),
+          )),
     );
   }
 }
-class ContainerWithBoxDecorationWidget extends StatelessWidget{
+
+class ContainerWithBoxDecorationWidget extends StatelessWidget {
   const ContainerWithBoxDecorationWidget({
     Key key,
-}): super(key: key);
+  }) : super(key: key);
 
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         Container(
           height: 100,
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(100.0),
-              bottomRight: Radius.circular(10.0),
-            ),
-          gradient : LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Colors.white,
-              Colors.lightGreen.shade500,
-            ]
-          ),
-            boxShadow: [
-              BoxShadow(
-                color:  Colors.grey,
-                blurRadius: 10,
-                offset: Offset(0,10)
-              )
-            ]
-          ),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(100.0),
+                bottomRight: Radius.circular(10.0),
+              ),
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.white,
+                    Colors.lightGreen.shade500,
+                  ]),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey, blurRadius: 10, offset: Offset(0, 10))
+              ]),
           child: Center(
             child: RichText(
-                text: TextSpan(
-                    text: 'Flutter World',
+              text: TextSpan(
+                  text: 'Flutter World',
                   style: TextStyle(
                     fontSize: 24.0,
                     color: Colors.deepPurple,
@@ -104,17 +106,87 @@ class ContainerWithBoxDecorationWidget extends StatelessWidget{
                       text: ' for',
                     ),
                     TextSpan(
-                      text: ' Mobile',
-                      style: TextStyle(
-                        color: Colors.deepOrange,
-                        fontWeight: FontWeight.bold,
-                      )
-                    )
-                  ]
-                ),
+                        text: ' Mobile',
+                        style: TextStyle(
+                          color: Colors.deepOrange,
+                          fontWeight: FontWeight.bold,
+                        ))
+                  ]),
             ),
           ),
         ),
+      ],
+    );
+  }
+}
+
+class ColumnWidget extends StatelessWidget {
+  const ColumnWidget({
+    Key key,
+  }) : super(key: key);
+
+  Widget build(BuildContext context) {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Text('Column 1'),
+          Divider(),
+          Text('Column 2'),
+          Divider(),
+          Text('Column 3'),
+        ]);
+  }
+}
+
+class RowWidget extends StatelessWidget {
+  const RowWidget({
+    Key key,
+  }) : super(key: key);
+
+  Widget build(BuildContext context){
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Text('Row 1'),
+            Padding(padding: EdgeInsets.all(16.0)),
+            Text('Row 2'),
+            Padding(padding: EdgeInsets.all(16.0),),
+            Text('Row 3'),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class ColumnAndRowNestingWidget extends StatelessWidget{
+  const ColumnAndRowNestingWidget({
+    Key key,
+}): super(key: key);
+
+  Widget build(BuildContext context){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Text('Column and Row Nesting 1'),
+        Text('Columns and Row Nesting 2'),
+        Text('Columns and Row Nesting 3'),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text('Row Nesting 1'),
+            Text('Row Nesting 2'),
+            Text('Row Nesting 3'),
+          ],
+        )
       ],
     );
   }
