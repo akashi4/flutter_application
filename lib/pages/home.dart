2bb6a0f8ef209c70c5b2ba5d4d7a9d1f3b91cd90
DD <aka@email.cm>
2020-12-08 18:21:33 +0100

    buttonBar and some buttonWidget

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -30,52 +31,46 @@ class _HomeState extends State<Home> {
             margin: EdgeInsets.fromLTRB(0, 0, 0, 20.0),
           ),
         ),
-        bottom: PreferredSize(
-          child: Container(
-            color: Colors.lightGreen.shade100,
-            height: 75.0,
-            width: double.infinity,
-            child: Center(
-              child: Text('Bottom'),
-            ),
-          ),
-          preferredSize: Size.fromHeight(75.0),
-        ),
+        bottom: PopupMenuButtonWidget(),
       ),
       body: Padding(
           padding: EdgeInsets.all(16),
           child: SafeArea(
             child: SingleChildScrollView(
               child: Column(
-                children: [
+                children: <Widget>[
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      FlatButton(onPressed: null, child: Text('Flag')),
-                      FlatButton(onPressed: null, child: Icon(Icons.flag),color: Colors.lightGreen, textColor: Colors.white,),
+                      FlatButton(onPressed: (){}, child: Text('Flag')),
+                      Padding(padding: EdgeInsets.all(10),),
+                      FlatButton(onPressed: (){}, child: Icon(Icons.flag),color: Colors.lightGreen, textColor: Colors.white,),
                     ],
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      RaisedButton(onPressed: null,child: Text('Save'),),
+                      RaisedButton(onPressed: (){},child: Text('Save'),),
                       Padding(padding: EdgeInsets.all(10)),
-                      RaisedButton(onPressed: null,child: Icon(Icons.save), color: Colors.lightGreen,)
+                      RaisedButton(onPressed: (){},child: Icon(Icons.save), color: Colors.lightGreen,)
                       //Button()
                     ],
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      IconButton(icon: Icon(Icons.flight), onPressed: null),
+                      IconButton(icon: Icon(Icons.flight), onPressed: (){}),
                       Padding(padding: EdgeInsets.all(10),),
-                      IconButton(icon: Icon(Icons.flight), onPressed: null, iconSize: 42.0,color: Colors.white, tooltip: 'Flight',),
+                      IconButton(icon: Icon(Icons.flight), onPressed: (){}, iconSize: 42.0,color: Colors.grey, tooltip: 'Flight',),
                     ],
-                  )
+                  ),
+                  const PopupMenuButtonWidget(),
+                  const ButtonWidget(),
+                  const ButtonBarWidget(),
                 ],
               ),
             ),
-          )
+          ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
       floatingActionButton: FloatingActionButton(
@@ -84,6 +79,79 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.lightGreen.shade100,
       ),
       bottomNavigationBar: const bottomNavBarWidget(),
+    );
+  }
+}
+
+class ButtonBarWidget extends StatelessWidget{
+  const ButtonBarWidget({Key key}): super(key: key);
+
+  Widget build(BuildContext context){
+    return Container(
+      color: Colors.white70,
+      child: ButtonBar(
+        alignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          IconButton(icon: Icon(Icons.airport_shuttle), onPressed: (){}),
+          IconButton(icon: Icon(Icons.map), onPressed: (){}),
+          IconButton(icon: Icon(Icons.brush), onPressed: (){},highlightColor: Colors.purple,)
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonWidget extends StatelessWidget{
+  const ButtonWidget({ Key key}): super(key: key);
+
+  // Widget for a set of buttons
+
+  Widget build(BuildContext context){
+    return Column(
+      children: [
+        Row(
+          children: [
+            Padding(padding: EdgeInsets.all(16)),
+            FlatButton(
+              onPressed: (){},
+              child: Text('Flag'),
+            ),
+            Padding(padding: EdgeInsets.all(16)),
+            FlatButton(
+              onPressed: (){},
+              child: Icon(Icons.flag),
+              color: Colors.lightGreen,
+              textColor: Colors.white,
+            ),
+          ],
+        ),
+        Divider(),
+        Row(
+          children: [
+            Padding(padding: EdgeInsets.all(16)),
+            RaisedButton(
+                onPressed: (){},
+                child: Text('Save'),
+            ),
+            Padding(padding: EdgeInsets.all(16)),
+            RaisedButton(
+                onPressed: (){},
+                child: Icon(Icons.save),
+                color: Colors.lightGreen,
+            ),
+          ],
+        ),
+        Divider(),
+        Row(
+          children: [
+            Padding(padding: EdgeInsets.all(16)),
+            IconButton(icon: Icon(Icons.flight), onPressed: (){}),
+            Padding(padding: EdgeInsets.all(16),),
+            IconButton(icon: Icon(Icons.flight), onPressed: (){},iconSize: 42,color: Colors.lightGreen, tooltip: 'Flight'),
+            Divider(),
+          ],
+        )
+      ],
     );
   }
 }
@@ -110,3 +178,50 @@ class bottomNavBarWidget extends StatelessWidget{
   }
 }
 
+class TodoMenuItem {
+  final String title;
+  final Icon icon;
+
+  TodoMenuItem({this.title, this.icon});
+
+}
+
+class PopupMenuButtonWidget extends StatelessWidget implements PreferredSizeWidget{
+  const PopupMenuButtonWidget({ Key key }): super(key: key);
+
+  Widget build(BuildContext context){
+    return Container(
+      color: Colors.lightGreen.shade100,
+      height: preferredSize.height,
+      width: double.infinity,
+      child: Center(
+        child: PopupMenuButton<TodoMenuItem>(
+            icon: Icon(Icons.view_list),
+            onSelected: ((valueSelected) { print('valueSelected : ${valueSelected.title}');}),
+            itemBuilder: (BuildContext context){
+              return foodMenuList.map((TodoMenuItem todoMenuItem){
+                return PopupMenuItem<TodoMenuItem>(
+                  value: todoMenuItem,
+                  child: Row(
+                    children: [
+                      Icon(todoMenuItem.icon.icon),
+                      Padding(padding: EdgeInsets.all(8),),
+                      Text(todoMenuItem.title),
+                    ],
+                  ),
+                );
+              }).toList();
+            }),
+      ),
+    );
+  }
+
+  Size get preferredSize => Size.fromHeight(75.0);
+}
+
+List <TodoMenuItem> foodMenuList = [
+  TodoMenuItem(title: 'Fast Food', icon: Icon(Icons.fastfood)),
+  TodoMenuItem(title: 'Remind me', icon: Icon(Icons.add_alarm)),
+  TodoMenuItem(title: 'Flight', icon: Icon(Icons.flight)),
+  TodoMenuItem(title: 'Music', icon: Icon(Icons.audiotrack)),
+];
